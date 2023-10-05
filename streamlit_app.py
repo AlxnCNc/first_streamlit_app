5717b9b42e12d34b5b65036bde6754b108877243
AlxnCNc <147084805+AlxnCNc@users.noreply.github.com>
2023-10-05 09:27:37 -0700

    Update streamlit_app.py
    Added menu options.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boilrd Free-Range Egg')
